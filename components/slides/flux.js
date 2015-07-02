1cf8a572ac6a97bb1b1533ae76544ddbb95158e4
Den <dfitsak@gmail.com>
2015-07-02 08:07:10 +0200

    more material

--- components/slides/flux.js
+++ components/slides/flux.js
@@ -1,16 +1,29 @@
 var React = require('react');
 
 var Flux = React.createClass({
+    imgStyles: {
+        'width': '1000px',
+        'margin-bottom': '10%',
+        'margin-left': 'auto'
+    },
 	render: function() {
 		return (
 			<div>
-				<h2>Flux</h2>
-				<ul>
-					<li>Dispatcher</li>
-					<li>Store</li>
-					<li>Actions</li>
-					<li>View(React)</li>
-				</ul>
+                <div className="overlay">
+                    <h2>Flux</h2>
+                    <ul>
+                        <li>Dispatcher</li>
+                        <li>Store</li>
+                        <li>Actions</li>
+                        <li>View(React)</li>
+                    </ul>
+                </div>
+                <div>
+                    <img
+                        style={this.imgStyles}
+                        src="http://fluxxor.com/images/flux-complex.png"
+                    />
+                </div>
 			</div>
 		);
 	}
